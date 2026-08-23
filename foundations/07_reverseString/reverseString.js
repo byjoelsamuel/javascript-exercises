ba0129207e203f2480bb81a6282a8b2cf484540e
Joel Samuel <byjoelsamuel@gmail.com>
2026-08-22 20:54:07 -0700

    Add solutions to 06 AND 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+  const reverseString = function (string) {
+    let reversedString = "";
 
-};
+    // reverse order, and print backwards
+    for (let i = string.length -1; i >= 0; i--) {
+      reversedString += string[i];
+    }
+    return reversedString;
+  };
 
 // Do not edit below this line
 module.exports = reverseString;
